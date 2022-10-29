05c8e97022ab0595d1b468e2227621dbf275ff9a
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-10-29 16:29:52 +0200

    arrays solo falta fotos

--- U3T6/arrays.js
+++ U3T6/arrays.js
@@ -12,40 +12,168 @@ switch (opciones) {
     MostrarListadoPaises(array);
     break;
     case 3:
-        
+    IntroduceIntervalos(array);
     break;
     case 4:
-        
+    AnadirPais(array);
     break;
     case 5:
-        
+    Borrar(array);
     break;
     case 6:
-        
+    Consultar(array);
     break;
     default:
         alert("no es una opcion valida");
 }
 
 
+function MostrarListadoPaises(array){
+    let pregunta = parseInt(prompt("¿como quieres mostrar los paises?\n1-mostrar sin orden\n2-mostrar del reves\n3-Alfabéticamente"));
+    switch(pregunta){
+        case 1:
+            MostrarArray(array);
+            break;
+        case 2:
+            ElementosAlreves(array);
+            break;
+        case 3:
+            ElementosAlfabeticamente(array);
+            break;
+        default:
+            alert("no es una opcion valida");
+    }
+
+}
+
+function IntroduceIntervalos(array){
+    let inicio = prompt("Introduzca el inicio");
+    let final = prompt("introduzca el final");
+    Intervalo(array, inicio, final)
+}
+
+
+function AnadirPais(array){
+let pregunta = parseInt(prompt("¿Donde quieres introducir el pais?\n1-al principio\n2-al final"));
+let pais="";
+switch(pregunta){
+    case 1:
+        pais = prompt("introduce un pais");
+        AnadirInicio(array, pais);
+        break;
+    case 2:
+        pais = prompt("introduce un pais");
+        AnadirFinal(array, pais);
+        break;
+    default:
+        alert("no es una opcion valida");
+}
+
+}
+
+function Borrar(array){
+let pregunta = parseInt(prompt("¿Donde quieres borrar?\n1-al principio\n2-al final"));
+switch(pregunta){
+    case 1:
+        BorrarPrincipio(array);
+        break;
+    case 2:
+        BorrarFinal(array);
+        break;
+    default:
+        alert("no es una opcion valida");
+}
+}
+
+function Consultar(array){
+let pregunta = parseInt(prompt("¿Por que quieres consultar?\n1-por posición\n2-por nombre"));
+let posicion= 0;
+let nombre="";
+switch(pregunta){
+    case 1:
+        posicion = prompt("introduce la posición");
+        ConsultarPosicion(array, posicion);
+        break;
+    case 2:
+        nombre = prompt("introduce el nombre");
+        ConsultarNombre(array, nombre);
+        break;
+    default:
+        alert("no es una opcion valida");
+}
+}
+
+/*funciones internas*/
+
 function MostrarNElementos(array){
     console.log(array.length);
     document.write("<p>Numero de paises: " + array.length + "</p>");
 }
 
-function MostrarListadoPaises(array){
-    let pregunta = parseInt(prompt("¿como quieres mostrar los paises?\n1-mostrar sin orden\n2-mostrar del reves\n3-Alfabéticamente"));
-    switch(pregunta){
-        case 1:
-            document.write("<p>Listado de paises sin ordenar:" + array +  "</p>");
+function MostrarArray(array){
+    document.write("<p>Listado de paises sin ordenar:" + array +  "</p>");
+}
+
+function ElementosAlreves(array){
+    document.write("<p>Listado de paises del reves:" + array.reverse() +  "</p>");
+    console.log(array);
+}
+
+function ElementosAlfabeticamente(array){
+    let nuevoarray = Array(array.length);
+    for(let i =0; i< array.length; i++){
+        nuevoarray[i] = array[i];
+    }
+    document.write("<p>Listado de paises alfabéticamente:" + nuevoarray.sort() +  "</p>");
+    console.log(nuevoarray);
+    console.log(array);
+}
+
+function AnadirInicio(array, pais){
+    array.unshift(pais);
+    document.write("<p>As añadido al princio: " + pais +  "</p>");
+    document.write("<p>resultado: " + array +  "</p>");
+}
+
+function AnadirFinal(array, pais){
+    array.push(pais);
+    document.write("<p>As añadido al princio: " + pais +  "</p>");
+    document.write("<p>resultado: " + array +  "</p>");
+}
+
+function BorrarPrincipio(array){
+    let principio = array[0];
+    array.shift();
+    document.write("<p>borraste: " + principio +  "</p>");
+    document.write("<p>resultado: " + array +  "</p>");
+}
+
+function BorrarFinal(array){
+    let final = array[array.length -1];
+    array.pop();
+    document.write("<p>borraste: " + final +  "</p>");
+    document.write("<p>resultado: " + array +  "</p>");
+}
+
+function Intervalo(array, inicio, final){
+    document.write("<p>intervalo de paises:" + array.slice(inicio, final) +  "</p>");
+}
+
+function ConsultarPosicion(array, posicion){
+    document.write("<p>El pais de la posicion " + posicion + " es: " + array[posicion] + "</p>");
+}
+
+function ConsultarNombre(array, nombre){
+    let existe=false;
+    for(let i =0; i< array.length; i++){
+        if(array[i]== nombre){
+            document.write("<p>El pais " + nombre + " tiene la posicion: " + (i+1) + "</p>");
+            existe = true;
             break;
-        case 2:
-            
-            break;
-        case 3:
-            break;
-        default:
-            alert("no es una opcion valida");
+        }
+    }
+    if(existe == false){
+        document.write("<p>El pais " + nombre + " no esta registrado </p>");
     }
 
 }
