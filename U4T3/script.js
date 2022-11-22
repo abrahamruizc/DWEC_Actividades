6fbeb5b2d2a3576cd4e067f7f43fda0e2f46cda1
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-11-22 19:46:45 +0100

    haciendo U4T3

--- U4T3/script.js
+++ U4T3/script.js
@@ -1,8 +1,22 @@
+window.onload = addEventListener("submit", validaciones);
 
-function validarNombre(){
-
-}
-
-function validarGrupomusica(){
+function validaciones(){
+    let nombre = document.getElementById("nombre").value;
+    let grupo = document.getElementById("grupo").value;
+    let ano = document.getElementById("ano").value;
+    let localizacion = document.getElementById("localizacion").value;
     
+    if(compruebaNombreGrupo(nombre) == false|| nombre ==""){
+        alert("el nombre no es correcto");
+    } else if (compruebaNombreGrupo(grupo) == false|| grupo ==""){
+        alert("el grupo no es correcto");
+    } 
+
+
 }
+
+function compruebaNombreGrupo(texto) {
+    const validacion = /^[a-zA-Z0-9]{0,20}$/;
+    return validacion.test(texto);
+}
+
