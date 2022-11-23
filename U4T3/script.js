b441ed3516a10941f651247d113defc5e73aecd4
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-11-23 19:31:01 +0100

    U4T3 hecho

--- U4T3/script.js
+++ U4T3/script.js
@@ -1,4 +1,9 @@
-window.onload = addEventListener("submit", validaciones);
+window.onload = iniciarEventos;
+
+function iniciarEventos(){
+   const enviar = document.getElementById("enviar");
+   enviar.addEventListener("click", validaciones);
+}
 
 function validaciones(){
     let nombre = document.getElementById("nombre").value;
@@ -6,17 +11,39 @@ function validaciones(){
     let ano = document.getElementById("ano").value;
     let localizacion = document.getElementById("localizacion").value;
     
-    if(compruebaNombreGrupo(nombre) == false|| nombre ==""){
+    if(campo20(nombre) == false|| nombre ==""){
         alert("el nombre no es correcto");
-    } else if (compruebaNombreGrupo(grupo) == false|| grupo ==""){
+    } else if (campo20(grupo) == false|| grupo ==""){
         alert("el grupo no es correcto");
-    } 
+    } else if (compruebaAno(ano) == false || ano ==""){
+        alert("el año de publicación no es correcto");
+    } else if (compruebaLocalizacion(localizacion) == false){
+        alert("la localización no es correcta");
+    }
 
-
+    if(campo20(nombre) && campo20(grupo) && compruebaAno(ano) && compruebaLocalizacion(localizacion)){
+        eliminarEventos();
+    }
 }
 
-function compruebaNombreGrupo(texto) {
+function eliminarEventos(){
+    enviar.removeEventListener("click", validaciones);
+    alert("se elimina evento")
+}
+
+function campo20(texto) {
     const validacion = /^[a-zA-Z0-9]{0,20}$/;
     return validacion.test(texto);
 }
+
+function compruebaAno(ano){
+    const validacion = /^[0-9]{4}$/;
+    return validacion.test(ano);
+}
+
+function compruebaLocalizacion(localizacion){
+    const validacion = /^[0-9]?$/;
+    return validacion.test(localizacion);
+}
+
 
