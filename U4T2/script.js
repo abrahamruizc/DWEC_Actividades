228b10192defa1ba5a43f8dd5e4a37e95f295c8f
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-11-29 20:27:07 +0100

    seguir haciendo

--- U4T2/script.js
+++ U4T2/script.js
@@ -1,26 +1,31 @@
 const cuadros = document.querySelectorAll("td");
+const imagenes = document.querySelectorAll("img");
 
 for(i = 0; i< cuadros.length; i++){
-    // let clase = cuadros[i].firstChild.className;
-    // console.log(clase);
-    cuadros[i].addEventListener("click", function (){
-        this.firstChild.style.display = "inline";
-        let clase = this.firstChild.className;
-        let contador= 0;
-        for(e =0; e < cuadros.length; e++){
-            if(cuadros[e].firstChild.className == clase && cuadros[e].firstChild.style.display == "inline"){
-                contador++;
-                console.log(contador);
-                if(contador ==2){
-                    cuadros[e].style.backgroundColor = "green";
-                    this.style.backgroundColor= "green";
-                }
-
-            }
-
-        }
-    });
+    cuadros[i].addEventListener("click", comprobarimagenes);
 }
+
+function comprobarimagenes(){
+    this.firstChild.style.display = "inline";
+    let clase = this.firstChild.className;
+    console.log(clase);
+    let contador= 0;
+    for(e =0; e < cuadros.length; e++){
+        if(cuadros[e].firstChild.className == clase && cuadros[e].firstChild.style.display == "inline"){
+            contador++;
+            console.log(contador);
+            if(contador ==2){
+                cuadros[e].style.backgroundColor = "green";
+                this.style.backgroundColor= "green";
+            }
+        } else {
+            cuadros[e].firstChild.style.display = "none";
+        }
+
+    }
+}
+
+
 
 
 // for(const entry of cuadros.entries()){
