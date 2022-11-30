63270a45818d37390343a8e4a265e1148fe43eba
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-11-30 13:02:26 +0100

    solo falta puntuacion

--- U4T2/script.js
+++ U4T2/script.js
@@ -1,28 +1,57 @@
 const cuadros = document.querySelectorAll("td");
-const imagenes = document.querySelectorAll("img");
+let puntuacion = parseInt(document.getElementById("puntuacion").value);
+
+console.log(puntuacion)
 
 for(i = 0; i< cuadros.length; i++){
     cuadros[i].addEventListener("click", comprobarimagenes);
 }
 
 function comprobarimagenes(){
-    this.firstChild.style.display = "inline";
+    let imagen = this.firstChild.style.display = "inline";
+    console.log(imagen)
     let clase = this.firstChild.className;
     console.log(clase);
-    let contador= 0;
-    for(e =0; e < cuadros.length; e++){
-        if(cuadros[e].firstChild.className == clase && cuadros[e].firstChild.style.display == "inline"){
-            contador++;
-            console.log(contador);
-            if(contador ==2){
-                cuadros[e].style.backgroundColor = "green";
-                this.style.backgroundColor= "green";
-            }
-        } else {
-            cuadros[e].firstChild.style.display = "none";
+    contadoriguales= 0;
+    contadorinlines=0;
+    let verdes=0;
+    for(e = 0; e < cuadros.length; e++){
+        if(cuadros[e].firstChild.style.display=="inline"){
+            contadorinlines++;
+            console.log(contadorinlines)
         }
+        if(cuadros[e].firstChild.className == clase && cuadros[e].firstChild.style.display == imagen){
+            contadoriguales++;
+            verdes++;
+            console.log(contadoriguales) 
+        } 
+
+        if(contadorinlines == 2){
+            contadorinlines=0;
+            for(o = 0; o < cuadros.length; o++){
+                  if(cuadros[o].firstChild.className == clase && cuadros[o].firstChild.style.display != imagen) {
+                    for(c = 0; c < cuadros.length; c++){
+                        cuadros[c].firstChild.style.display = "none";
+                    }
+                }
+            }
+        }
+        if(contadoriguales ==2){
+            //puntuacion= puntuacion + 1;
+            for(o = 0; o < cuadros.length; o++){
+                if(cuadros[o].firstChild.className== clase){
+                    cuadros[o].removeEventListener("click", comprobarimagenes);
+                    cuadros[o].style.backgroundColor = "green";
+                    cuadros[o].firstChild.style.display ="none";
+                    
+                }
+            }
+        }
+        
 
     }
+    
+    //document.getElementById("puntuacion").value = puntuacion;
 }
 
 
