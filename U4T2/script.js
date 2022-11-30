6f6d70d44068a0ccda0b8294343c25a424a65721
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-11-30 13:35:48 +0100

    EJERCICIO DE LAS CARTAS HECHOOO

--- U4T2/script.js
+++ U4T2/script.js
@@ -3,13 +3,17 @@ let puntuacion = parseInt(document.getElementById("puntuacion").value);
 
 console.log(puntuacion)
 
-for(i = 0; i< cuadros.length; i++){
-    cuadros[i].addEventListener("click", comprobarimagenes);
-}
+setTimeout(() => {
+    for(i = 0; i< cuadros.length; i++){
+        cuadros[i].firstChild.style.display ="none";
+        cuadros[i].addEventListener("click", comprobarimagenes);
+    }
+},1000);
+
 
 function comprobarimagenes(){
     let imagen = this.firstChild.style.display = "inline";
-    console.log(imagen)
+    console.log(imagen);
     let clase = this.firstChild.className;
     console.log(clase);
     contadoriguales= 0;
@@ -18,18 +22,25 @@ function comprobarimagenes(){
     for(e = 0; e < cuadros.length; e++){
         if(cuadros[e].firstChild.style.display=="inline"){
             contadorinlines++;
-            console.log(contadorinlines)
+            console.log(contadorinlines);
         }
         if(cuadros[e].firstChild.className == clase && cuadros[e].firstChild.style.display == imagen){
             contadoriguales++;
             verdes++;
-            console.log(contadoriguales) 
+            console.log(contadoriguales);
         } 
+
+
+        if(verdes == 2){
+            verdes=0;
+            puntuacion= (puntuacion + 1);
+            document.getElementById("puntuacion").value = puntuacion;
+        }
 
         if(contadorinlines == 2){
             contadorinlines=0;
             for(o = 0; o < cuadros.length; o++){
-                  if(cuadros[o].firstChild.className == clase && cuadros[o].firstChild.style.display != imagen) {
+                if(cuadros[o].firstChild.className == clase && cuadros[o].firstChild.style.display != imagen) {
                     for(c = 0; c < cuadros.length; c++){
                         cuadros[c].firstChild.style.display = "none";
                     }
@@ -37,7 +48,6 @@ function comprobarimagenes(){
             }
         }
         if(contadoriguales ==2){
-            //puntuacion= puntuacion + 1;
             for(o = 0; o < cuadros.length; o++){
                 if(cuadros[o].firstChild.className== clase){
                     cuadros[o].removeEventListener("click", comprobarimagenes);
@@ -51,13 +61,6 @@ function comprobarimagenes(){
 
     }
     
-    //document.getElementById("puntuacion").value = puntuacion;
+    
 }
-
-
-
-
-// for(const entry of cuadros.entries()){
-//     alert("mensaje")
-// }
 
