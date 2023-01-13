d35a04cec6fdc9279260dd3951073ae691743f71
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2023-01-13 17:55:15 +0100

    U6T2 Hecho

--- U6T2/script.js
+++ U6T2/script.js
@@ -3,10 +3,36 @@ window.onload = inicio;
 function inicio() {
     let boton = document.getElementById("boton");
     boton.addEventListener("click",pulsar);
+    let borrarp = document.getElementById("borrarp");
+    borrarp.addEventListener("click",borrarprimero);
+    let borraru = document.getElementById("borraru");
+    borraru.addEventListener("click",borrarultimo);
+}
+
+function borrarprimero(){
+    let lista = document.getElementById("lista");
+
+    lista.removeChild(lista.firstElementChild);
+
+}
+
+function borrarultimo(){
+    let lista = document.getElementById("lista");
+
+    lista.removeChild(lista.lastChild);
 }
 
 function pulsar(){
 
-    prompt("introduce un texto");
+    let texto = prompt("introduce un texto");
+
+    let li = document.createElement("li");
+    li.textContent = texto;
+
+    let lista = document.getElementById("lista");
+
+    lista.appendChild(li);
+
+    console.log(li);
 
 }
