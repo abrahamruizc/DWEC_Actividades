d88e0640959c7b51c82583fed6d0d6f86b64f360
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2023-01-18 19:28:00 +0100

    Haciendo U6T5

---
+++ U6T5/script.js
@@ -0,0 +1,31 @@
+window.onload = inicio;
+
+
+function inicio(){
+
+let div1 = document.createElement("div");
+div1.setAttribute("class", "grid__column_space1of4-3Auw grid__column-2zuc");
+
+let div2 = document.createElement("div");
+div2.setAttribute("class", "grid__card-1AMl grid__is_not_ad-3CXE");
+
+let article1 = document.createElement("article");
+article1.setAttribute("class", "cards__postcard-37d3 cards__postcardLandscape-3RIF cards__font_landscape_single_text_below_4_col-7iX7 cards__columns-4-YOiO background_color_zeta cards__no_has_section-3wNM");
+
+let div3 = document.createElement("div");
+
+let img1= document.createElement("img");
+img1.title="";
+img1.setAttribute("class", "cards__snap_image-aOud");
+img1.loading = "lazy";
+img1.src = "https://baul.mediaset.es/dist/assets/guides/img_guide.png?w=1024";
+img1.srcset ="https://baul.mediaset.es/dist/assets/guides/img_guide.png?w=1024 1024w,https://baul.mediaset.es/dist/assets/guides/img_guide.png?w=800   800w,https://baul.mediaset.es/dist/assets/guides/img_guide.png?w=768   768w,https://baul.mediaset.es/dist/assets/guides/img_guide.png?w=640   640w,https://baul.mediaset.es/dist/assets/guides/img_guide.png?w=600   600w,https://baul.mediaset.es/dist/assets/guides/img_guide.png?w=480   480w,https://baul.mediaset.es/dist/assets/guides/img_guide.png?w=360   360w,https://baul.mediaset.es/dist/assets/guides/img_guide.png?w=320   320w";
+img1.sizes = "(min-width: 960px) 502px, (min-width: 768px) 50vw, 100vw";
+img1.alt ="";
+img1.setAttribute("pinger-seen","true");
+
+let div4 = document.createElement("div");
+div4.setAttribute("class", "cards__postcard__content-1w21");
+
+
+}
