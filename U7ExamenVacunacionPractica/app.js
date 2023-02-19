26c4ab8ef80d7e2a6eaea2dd329ce8ed0beb67f3
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2023-02-19 14:44:13 +0100

    repasando examen vacunacion

--- U7ExamenVacunacionPractica/app.js
+++ U7ExamenVacunacionPractica/app.js
@@ -27,20 +27,76 @@ function cargarTabla(){
         let datos = JSON.parse(xml.responseText);
         console.log(datos);
 
-        document.getElementById("tabla").innerHTML = "";
+        let tabla_resultado = document.getElementById("tabla");
+        tabla_resultado.innerHTML = "";
         let tabla = document.createElement("table");
+        let fila = document.createElement("tr");
+        let comunidad;
+        let encabezado = ["ccaa","dosisEntregadas","dosisAdministradas","dosisPautaCompletada","porcentajeEntradas","porcentajePoblacionAdministradas","porcentajePoblacionCompletas"];
+        for (let index = 0; index < encabezado.length; index++) {
+            comunidad = document.createElement("th");
+            comunidad.textContent = encabezado[index];
+            fila.appendChild(comunidad);
+        }
+        
+        console.log(fila);
+        tabla.appendChild(fila);
         let tr = document.createElement("tr");
-        let comunidad = document.createElement("th");
+        let td;
+
+        for (let index = 0; index < datos.length; index++) {
+            comunidad = datos[index];
+            if(comunidad.ccaa != "Totales"){
+
+                let json_resultado = {
+                    ccaa: comunidad.ccaa,
+                    dosisEntregadas: comunidad.dosisEntregadas,
+                    dosisAdministradas: comunidad.dosisAdministradas,
+                    dosisPautaCompletada: comunidad.dosisPautaCompletada,
+                    porcentajeEntradas: comunidad.porcentajeEntradas,
+                    porcentajePoblacionAdministradas: comunidad.porcentajePoblacionAdministradas,
+                    porcentajePoblacionCompletas: comunidad.porcentajePoblacionCompletas
+                }
+
+                console.log(json_resultado);
+                
+                td = document.createElement("td");
+                
+                for (let index = 0; index < json_resultado.length; index++) {
+                    console.log(json_resultado[index]); 
+                    
+                }
+
+               for (const key in comunidad) {
+                let resultado = document.createTextNode(comunidad[key]);
+                //console.log(resultado);
+                td.textContent = resultado;
+                //console.log(td);
+                tr.appendChild(td);
+                
+               }
+                
+               
+                tabla.appendChild(tr);
+                //td.textContent = comunidad.ccaa;
+                //console.log(comunidad);
+
+
+
+            }
+        }
+
+            
+
+            
+
+            
+
+
         
-
-        tabla.appendChild(tr);
         
-
-
-        datos.forEach(comunidad => {
-
-
-        });
+        
+        tabla_resultado.appendChild(tabla);
 
     }
 }
